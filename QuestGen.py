de5a5b72297ff9660901263ec7a6ada4967281af
Lucas Schmitt <luckyblackskypro@gmail.com>
2025-01-26 12:17:15 +0100

    added variables lieu, donneurquete et récompense

--- QuestGen.py
+++ QuestGen.py
@@ -1,27 +1,51 @@
 import random
 
-objectif_combat = ["Tuer 4 feu follets","Vaincre 10 gobelins","Exterminer 20 slimes"]
-objectif_exploration = ["Trouver un artefact","Chercher la fleur noire","Retrouver la courrone du roi"]
+objectif_combat = ["tuer 4 feu follets","vaincre 10 gobelins","exterminer 20 slimes"]
+donneurs_quetes_combat = ["Le chef du village", "Un guerrier blessé", "La guilde des aventuriers"]
+lieu_quete_combat = ["au Donjon de Stoleis", "à la Grotte de la montagne","dans le Marais enchanté"]
+recompenses_combat = ["100 pièces d'or", "Une épée en acier", "Un bouclier renforcé"]
+
+objectif_exploration = ["trouver un artefact","chercher la fleur noire","retrouver le casque magique"]
+donneurs_quetes_exploration = ["Un vieux sage", "Un cartographe royal", "Un marchand d'objets rares"]
+lieu_quete_exploration = ["dans la Forêt de Stoleis", "autour du Jardin du chateaux","dans les profondeurs du Marais enchanté"]
+recompenses_exploration = ["Une carte au trésor", "Un artefact ancien", "Des informations secrètes"]
+
 objectif_social = ["Ramener le renard blanc à son propriétaire","Aider à couper des légumes","Porter les sacs de minerais"]
+
 objectif_collecte =["Récolter 12 lapis lazuli","Couper 6 arbres","Récolter 20 épis de blé"]
+
 objectif_artisanat = ["Forger une épée","Taner 4 cuire","Faire fondre un minerai de fer"]
 
-modeles_quetes_combat = ["L'objectif de cette mission est de [objectif]","Afin de rassurer le village il vous ai demandé de [objectif]"]
-modeles_quetes_exploration = ["Votre but est de [objectif]","On vous a demander de [objectif]"]
+
+
+
+modeles_quetes_combat = ["[donneur_quete] vous donne l'objectif de [objectif] se trouvant [lieu_quete], Récompense : [recompense]."]
+modeles_quetes_exploration = ["[donneur_quete] vous annonce que votre but est de [objectif], rendez vous [lieu_quete], Récompense : [recompense]."]
 
 categories = ["Combat","Exploration"]
 categorie_choisie = random.choice(categories)
 
 if categorie_choisie == "Combat":
     objectif = random.choice(objectif_combat)
+    donneur_quete = random.choice(donneurs_quetes_combat)
+    lieu_quete = random.choice(lieu_quete_combat)
+    recompense = random.choice(recompenses_combat)
     modele_choisi = random.choice(modeles_quetes_combat)
 
 elif categorie_choisie == "Exploration":
     objectif = random.choice(objectif_exploration)
+    donneur_quete = random.choice(donneurs_quetes_exploration)
+    lieu_quete = random.choice(lieu_quete_exploration)
+    recompense = random.choice(recompenses_exploration)
     modele_choisi = random.choice(modeles_quetes_exploration)
 
 
-description_quete = modele_choisi.replace("[objectif]", objectif)
+description_quete = modele_choisi.replace("[objectif]", objectif) \
+                                 .replace("[lieu_quete]", lieu_quete) \
+                                 .replace("[donneur_quete]", donneur_quete) \
+                                 .replace("[recompense]", recompense)
+
+print(description_quete)
 
 # Générer le contenu HTML
 contenu_html = f"""
